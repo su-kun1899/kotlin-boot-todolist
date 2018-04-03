39411d4508f50b94e367996563cc2b2b1c042e77
su-kun1899 <higedrum.coz@gmail.com>
2018-04-03 23:45:02 +0900

    rename application class

--- src/main/kotlin/red/sukun1899/kotlin/boot/todolist/Application.kt
+++ src/main/kotlin/red/sukun1899/kotlin/boot/todolist/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinBootTodolistApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<KotlinBootTodolistApplication>(*args)
+    runApplication<Application>(*args)
 }
--- src/test/kotlin/red/sukun1899/kotlin/boot/todolist/ApplicationTests.kt
+++ src/test/kotlin/red/sukun1899/kotlin/boot/todolist/ApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class KotlinBootTodolistApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
