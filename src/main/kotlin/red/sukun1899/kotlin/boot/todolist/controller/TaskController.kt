9b65797872e85a26d1c6b649961f159ce60881f0
su-kun1899 <higedrum.coz@gmail.com>
2018-04-03 22:18:35 +0900

    thymeleafを使った画面で表示

--- src/main/kotlin/red/sukun1899/kotlin/boot/todolist/controller/TaskController.kt
+++ src/main/kotlin/red/sukun1899/kotlin/boot/todolist/controller/TaskController.kt
@@ -1,22 +1,26 @@
 package red.sukun1899.kotlin.boot.todolist.controller
 
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
 import red.sukun1899.kotlin.boot.todolist.data.Task
 
 /**
  * @author su-kun1899
  */
-@RestController
+@Controller
+@RequestMapping("tasks")
 class TaskController {
 
     @GetMapping("")
     fun index(model: Model): String {
-        val task = listOf(
+        val tasks = listOf(
                 Task(1, "障子を張り替える", false),
                 Task(2, "定期検診に行く", true)
         )
-        return task.toString()
+        model.addAttribute("tasks", tasks)
+
+        return "tasks/index"
     }
 }
