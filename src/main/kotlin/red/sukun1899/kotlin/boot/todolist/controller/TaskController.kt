f2df05682968747604850f1df86cd022304b06ad
su-kun1899 <higedrum.coz@gmail.com>
2018-04-08 17:55:36 +0900

    タスク作成処理を追加

--- src/main/kotlin/red/sukun1899/kotlin/boot/todolist/controller/TaskController.kt
+++ src/main/kotlin/red/sukun1899/kotlin/boot/todolist/controller/TaskController.kt
@@ -2,7 +2,10 @@ package red.sukun1899.kotlin.boot.todolist.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import red.sukun1899.kotlin.boot.todolist.repsitory.TaskRepository
 
@@ -24,5 +27,16 @@ class TaskController(private val taskRepository: TaskRepository) {
     @GetMapping("new")
     fun new(form: TaskCreateForm): String {
         return "tasks/new"
+    }
+
+    @PostMapping
+    fun create(@Validated form: TaskCreateForm,
+               bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors()) {
+            return "tasks/new"
+        }
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
     }
 }
