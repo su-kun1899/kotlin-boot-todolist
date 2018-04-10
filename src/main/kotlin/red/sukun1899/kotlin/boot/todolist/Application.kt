184c2ed70e90147c25269844d73192fd236ed543
su-kun1899 <higedrum.coz@gmail.com>
2018-04-11 00:01:59 +0900

    設定とテーブル作成

--- src/main/kotlin/red/sukun1899/kotlin/boot/todolist/Application.kt
+++ src/main/kotlin/red/sukun1899/kotlin/boot/todolist/Application.kt
@@ -1,10 +1,23 @@
 package red.sukun1899.kotlin.boot.todolist
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class Application
+class Application {
+    @Bean
+    fun commandLineRunner(jdbcTemplate: JdbcTemplate) = CommandLineRunner {
+        jdbcTemplate.execute("""
+            CREATE TABLE IF NOT EXISTS task (
+              id       BIGINT       PRIMARY   KEY AUTO_INCREMENT,
+              content  VARCHAR(100) NOT NULL,
+              done     BOOLEAN      NOT NULL  DEFAULT FALSE
+            )""")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
